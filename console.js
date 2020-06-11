4d84894127ca41be4e76aff42660025ea73f2a5d
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-06-11 20:56:06 +0500

    interval and console.log, current timer, sentry, enumeration

---
+++ console.js
@@ -0,0 +1,33 @@
+function timer(n) {
+    let t=n;
+    let seconds = setInterval(()=>{
+            if(t>0) {
+                console.log(t--);
+            } else {
+                clearInterval(seconds);
+                console.log('Boom!');
+            }
+        }, 1000);
+}
+// timer(3);
+
+function sentry(n) {
+    setInterval(()=>{
+        console.log(new Date().getSeconds());
+    }, n);
+}
+// sentry(1000);
+
+function iterator(cb,n) {
+    
+}
+// iterator('abc',1000);
+
+function enumeration(n1,n2) {
+    for(let i=n1; i<=n2; i++) {
+        console.log(i);
+    }
+}
+// enumeration(1,2);
+
+
